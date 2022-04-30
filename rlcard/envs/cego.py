b55bb8b159093c7959eb34c69c36767a3838765f
Philipp Oeschger <kontakt@philipp-oeschger.de>
2022-04-30 18:25:55 +0200

    more comments added

--- rlcard/envs/cego.py
+++ rlcard/envs/cego.py
@@ -14,6 +14,13 @@ DEFAULT_GAME_CONFIG = {
 
 class CegoEnv(Env):
     ''' Cego Environment
+
+    Instance Attributes:
+        - name (str): the name of the game
+        - default_game_config (dict): the default game config
+        - game (Game): the game instance
+        - state_shape (list): the shape of the state space
+        - action_shape (list): the shape of the action space
     '''
 
     def __init__(self, config):
@@ -23,23 +30,27 @@ class CegoEnv(Env):
         self.default_game_config = DEFAULT_GAME_CONFIG
         self.game = Game()
         super().__init__(config)
-        '''
-            plane 0: [0] own cards
-            plane 0: [1] valued cards
-            plane 0: [2] winner of trick
-            plane 0: [3] cards in trick
-            plane 0: [4] cards played
-            plane 0: [5]
-                [0-3]: who is part of the team
-                [4-7]: who wins the current round
-                [8-11]: player who started the trick round
-            plane 1: 54 - cards
-        '''
         self.state_shape = [[6, 54] for _ in range(self.num_players)]
         self.action_shape = [None for _ in range(self.num_players)]
 
-    def _extract_state(self, state):
-        """ variable setup"""
+    def _extract_state(self, state) -> OrderedDict:
+        ''' Extract the observation for each player
+
+        Parameters:
+            - state (dict): the state of the game
+
+        Observation Representation
+            - plane 0: [0] own cards
+            - plane 0: [1] valued cards
+            - plane 0: [2] winner of trick
+            - plane 0: [3] cards in trick
+            - plane 0: [4] cards played
+            - plane 0: [5]
+                - [0-3]: who is part of the team
+                - [4-7]: who wins the current round
+                - [8-11]: player who started the trick round
+            - plane 1: 54 - cards
+        '''
         extracted_state: dict = OrderedDict()
         legal_actions: OrderedDict = self._get_legal_actions()
         obs = np.zeros((6, 54), dtype=int)  # observation is a (6, 54) tensor
@@ -104,18 +115,25 @@ class CegoEnv(Env):
 
         return extracted_state
 
-    def get_payoffs(self):
+    def get_payoffs(self) -> np.NDArray:
+        ''' Get the payoffs of the players'''
+
         payoffs = self.game.get_payoffs()
         return np.array(payoffs)
 
     def _decode_action(self, action_id):
+        ''' Decode the action id into the action
+        '''
+
         legal_ids = self._get_legal_actions()
         if action_id in legal_ids:
             return ACTION_LIST[action_id]
 
         return ACTION_LIST[np.random.choice(legal_ids)]
 
-    def _get_legal_actions(self):
+    def _get_legal_actions(self) -> OrderedDict:
+        ''' Get the legal actions of the current state'''
+
         legal_actions = self.game.get_legal_actions()
         legal_ids = {ACTION_SPACE[action]: None for action in legal_actions}
         return OrderedDict(legal_ids)
