952e102c0b33b8c94968828ce4d31a913d8cdaf4
Philipp Oeschger <philipp.oeschger@hs-furtwangen.de>
2022-06-17 18:12:26 +0200

    update

---
+++ final_model_training/run_dqn.py
@@ -0,0 +1,143 @@
+import os
+
+import torch
+
+import rlcard
+from rlcard.agents import DQNAgent
+from rlcard.agents.random_agent import RandomAgent
+
+from rlcard.games.cego.utils import get_random_search_args, args_to_str, save_args_params
+
+from rlcard.utils import (
+    tournament,
+    reorganize,
+    MyLogger,
+    plot_curve,
+    get_device,
+    set_seed,
+)  # import some useful functions
+
+# arguments for the random search
+args = {
+    "log_dir": "final_models/dqn_cego_player_0",
+    "env_name": "cego",
+    "game_variant": "standard",
+    "game_activate_heuristic": True,
+    "game_judge_by_points": 0,
+    "game_train_players": [True, True, True, True],
+    "seed": 20,
+    "replay_memory_size": 100000,
+    "update_target_estimator_every": 10000,
+    "discount_factor": 0.95,
+    "epsilon_start": 1.0,
+    "epsilon_end": 0.1,
+    "epsilon_decay_steps": 100000,
+    "batch_size": 32,
+    "mlp_layers": [512, 512],
+    "num_eval_games": 1000,
+    "num_episodes": 1000000,
+    "evaluate_every": 50,
+    "save_model_every": 100,
+    "learning_rate": 1e-05,
+}
+
+
+def train(log_dir, env_name, game_variant, game_activate_heuristic,
+          game_judge_by_points, game_train_players, seed, replay_memory_size,
+          update_target_estimator_every, discount_factor,
+          epsilon_start, epsilon_end, epsilon_decay_steps,
+          batch_size, mlp_layers, num_eval_games,
+          num_episodes, evaluate_every, save_model_every, learning_rate):
+    # Check whether gpu is available
+    device = get_device()
+    print(device)
+
+    set_seed(seed)
+
+    # Make the environment with seed
+    env = rlcard.make(
+        env_name,
+        config={
+            'seed': seed,
+            'game_variant': game_variant,
+            'game_activate_heuristic': game_activate_heuristic,
+            'game_judge_by_points': game_judge_by_points,
+            'game_train_players': game_train_players
+        }
+    )
+
+    # # this is our DQN agent
+    dqn_agent = DQNAgent(
+        num_actions=env.num_actions,
+        state_shape=env.state_shape[0],
+        mlp_layers=mlp_layers,
+        device=device,
+        replay_memory_size=replay_memory_size,
+        update_target_estimator_every=update_target_estimator_every,
+        discount_factor=discount_factor,
+        epsilon_start=epsilon_start,
+        epsilon_end=epsilon_end,
+        epsilon_decay_steps=epsilon_decay_steps,
+        batch_size=batch_size,
+        learning_rate=learning_rate
+
+    )
+    agents = [dqn_agent]
+    for _ in range(1, env.num_players):
+        agents.append(RandomAgent(num_actions=env.num_actions))
+
+    env.set_agents(agents)  # set agents to the environment
+
+    checkpoint_count= 0          
+
+    # Start training
+    with MyLogger(log_dir) as logger:
+        for episode in range(num_episodes):
+
+            # Generate data from the environment
+            trajectories, payoffs = env.run(is_training=True)
+
+            # Reorganaize the data to be state, action, reward, next_state, done
+            trajectories = reorganize(trajectories, payoffs)
+
+            # Feed transitions into agent memory, and train the agent
+            for ts in trajectories[0]:
+                dqn_agent.feed(ts)
+
+            # Evaluate the performance.
+            if episode % evaluate_every == 0:
+                logger.log_performance(
+                    env.timestep,
+                    tournament(
+                        env,
+                        num_eval_games,
+                    )[0]
+                )
+            
+            if episode % save_model_every == 0:
+                logger.save_csv()
+                os.mkdir(log_dir+ "/checkpoint_"+str(checkpoint_count))
+                csv_path, fig_path = logger.csv_path, log_dir+ "/checkpoint_" + str(checkpoint_count)+"/fig.png"
+                save_path = os.path.join(log_dir+ "/checkpoint_"+str(checkpoint_count), 'model.pth')
+                torch.save(log_dir, save_path)
+                plot_curve(csv_path, fig_path, "DQN")
+                checkpoint_count += 1
+
+        # Get the paths
+        # csv_path, fig_path = logger.csv_path, logger.fig_path
+
+def init_search_set(random_search_folder):
+    res = set()
+    if os.path.exists(random_search_folder+ "/search_set.txt"):
+        with open(random_search_folder + "/search_set.txt", "r") as f:
+            search_set = set(f.read().splitlines())
+
+            for val in search_set:
+                res.add(val)
+
+    return res
+
+
+if __name__ == '__main__':
+    os.environ["CUDA_VISIBLE_DEVICES"] = "cpu"
+    train(**args)
--- rlcard/utils/__init__.py
+++ rlcard/utils/__init__.py
@@ -1,4 +1,5 @@
 from rlcard.utils.logger import Logger
+from rlcard.utils.MyLogger import MyLogger
 from rlcard.utils import seeding
 from rlcard.utils.utils import *
 from rlcard.utils.pettingzoo_utils import *
